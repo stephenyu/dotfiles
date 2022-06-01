8ad8de1c94a5b6747d480bed6345bfcddc1ff7a2
stephenyu <s.yu@less3.com>
2022-06-01 20:42:07 +0000

    Added Python

--- nvim/lua/luaconfig.lua
+++ nvim/lua/luaconfig.lua
@@ -1,4 +1,5 @@
 local nvim_lsp = require("lspconfig")
+-- local pyright = require("pyright")
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
@@ -115,7 +116,7 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- OnAttach Loop
-local servers = { 'tsserver' }
+local servers = { 'tsserver', 'pyright' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
